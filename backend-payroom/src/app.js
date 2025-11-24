f4af43f6ab2a84371db6c3c701c32029c8c32538
Nhat Bao Dinh <nhatbaodinh@Baos-MacBook.local>
2025-11-24 16:59:28 +0700

    Houses CRUD

--- backend-payroom/src/app.js
+++ backend-payroom/src/app.js
@@ -1,24 +1,28 @@
 import express from "express";
 import router from "./modules/auth/auth.route.js";
+import housesRouter from "./modules/houses/houses.route.js";
+import { authMiddleware } from "./middlewares/auth.js";
+import { allowRoles } from "./middlewares/role.js";
 
 const app = express();
 app.use(express.json());
 
 app.use("/auth", router);
+app.use("/houses", housesRouter);
 
 app.use((req, res, next) => {
-    if (req.url.endsWith("/") && req.url.length > 1) {
-        req.url = req.url.slice(0, -1);
-    }
-    next();
+  if (req.url.endsWith("/") && req.url.length > 1) {
+    req.url = req.url.slice(0, -1);
+  }
+  next();
 });
 // Test phan quyen
 app.get("/test/owner", authMiddleware, allowRoles("owner"), (req, res) => {
-    res.send("OWNER OK");
+  res.send("OWNER OK");
 });
 
 app.get("/test/tenant", authMiddleware, allowRoles("tenant"), (req, res) => {
-    res.send("TENANT OK");
+  res.send("TENANT OK");
 });
 
 export default app;

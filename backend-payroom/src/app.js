5a02174896fe209c5164e0f0c23dd9b2325e7ad6
baonhatdinh <bao.nhatdinh@infodation.vn>
2025-11-25 10:04:08 +0700

    Invoices feature

--- backend-payroom/src/app.js
+++ backend-payroom/src/app.js
@@ -4,6 +4,7 @@ import housesRouter from "./modules/houses/houses.route.js";
 import roomsRouter from "./modules/rooms/rooms.route.js";
 import tenantsRouter from "./modules/tenants/tenants.route.js";
 import contractsRouter from "./modules/contracts/contracts.route.js";
+import invoicesRouter from "./modules/invoices/invoices.route.js";
 import { authMiddleware } from "./middlewares/auth.js";
 import { allowRoles } from "./middlewares/role.js";
 
@@ -15,6 +16,7 @@ app.use("/houses", housesRouter);
 app.use("/rooms", roomsRouter);
 app.use("/tenants", tenantsRouter);
 app.use("/contracts", contractsRouter);
+app.use("/invoices", invoicesRouter);
 
 app.use((req, res, next) => {
   if (req.url.endsWith("/") && req.url.length > 1) {
